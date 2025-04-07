9085d51fe19f0ff100e14345cec8961a079b25dd
H033S <tonito.nazco@gmail.com>
2025-04-07 16:58:36 -0400

    Added test endpoint

--- api/app.py
+++ api/app.py
@@ -11,10 +11,18 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/report/")
+@app.post("/api/v1/report/")
 def generate_report(session_data: SessionData):
     gpt_handler = ChatGPTHandler()
 
     response = gpt_handler.query(session_data)
 
     return response
+
+@app.post("/api/test/report/")
+def generate_test_report(session_data: SessionData):
+
+    with open("./test_report.txt") as file:
+        dummy_report = file.read()
+
+        return dummy_report
